4b40ad3b6529772f1899014a28e192bdc0a6ebe0
Nathanael PINEAU <nofigh@hotmail.fr>
2013-12-09 19:06:48 +0100

    updt

--- man_sample.c
+++ man_sample.c
@@ -4,11 +4,22 @@
 #ifndef NOT_FOUND
 # define NOT_FOUND ("NOT FOUND")
 #endif
+#ifndef DIRENT_H
+# define DIRENT_H
+# include <dirent.h>
+#endif
+#include "libft.h"
 
-char	*ft_search_dir(char *name)
+char	*ft_search_dir(char *path, char *name)
 {
-	strlen(name);
-	dirp = opendir(".");
+	DIR				*dirp;
+	struct dirent	*dp;
+	int				len;
+
+	len = strlen(name);
+	dirp = opendir(path);
+	if (!dirp)
+		return NOT_FOUND;
 	while ((dp = readdir(dirp)) != NULL)
 		if (dp->d_namlen == len && !strcmp(dp->d_name, name)) {
 			(void)closedir(dirp);
@@ -20,6 +31,9 @@ char	*ft_search_dir(char *name)
 
 int		main(int argc, char **argv)
 {
-	if (argc > 1)
+	if (argc > 2)
 	{
-		ft_putendl(
+		ft_putendl(ft_search_dir(argv[1], argv[2]));
+	}
+	return (0);
+}
