80f67d5b25e4a3aedde13cf3318b80854422ccd1
Nathanael PINEAU <nofigh@hotmail.fr>
2013-12-12 16:13:20 +0100

    mise a jour du header : prototypes de print del_node et del_tree.

--- includes/ft_ls.h
+++ includes/ft_ls.h
@@ -6,7 +6,7 @@
 /*   By: npineau <npineau@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2013/12/11 12:57:31 by npineau           #+#    #+#             */
-/*   Updated: 2013/12/12 13:45:59 by npineau          ###   ########.fr       */
+/*   Updated: 2013/12/12 16:12:19 by npineau          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 #ifndef FT_LS_H
@@ -37,12 +37,17 @@ typedef struct		s_btree
 t_ddata	*ft_new_ddata(void);
 int		ft_cpy_data(t_ddata *dest, char *src);
 void	ft_del_ddata(t_ddata **trash);
+
 int		ft_gather_options(int argc, char **argv, t_opt *opt);
 void	ft_put_option(char c, t_opt *opt);
 int		ft_tri(t_btree *current, t_btree *new, t_opt *opt);
+void	ft_print(t_ddata *spec, t_opt *opt);
+
 t_btree	*ft_new_node(void);
 t_btree	*ft_place_node(t_btree *current, t_btree *new, t_opt *opt);
 void	ft_parkour(t_btree *current, t_opt *opt);
 void	ft_rev_parkour(t_btree *current, t_opt *opt);
+void	ft_del_node(t_btree **current);
+void	ft_del_tree(t_btree **current);
 
-#endif
+#endif /* !FT_LS_H */
