6e53a2f50b008d76f9ef2952233488a1ac67ab1c
Nathanael PINEAU <nofigh@hotmail.fr>
2013-12-12 13:26:29 +0100

    Ajout de ft_rev_parkour.

---
+++ sources/tree/ft_rev_parkour.c
@@ -0,0 +1,22 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_rev_parkour.c                                   :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: npineau <npineau@student.42.fr>            +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2013/12/12 13:24:55 by npineau           #+#    #+#             */
+/*   Updated: 2013/12/12 13:26:05 by npineau          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "ft_ls"
+
+void	ft_rev_parkour(t_btree *current, t_opt *opt)
+{
+	if (current->right != NULL)
+		ft_parkour(curent->right, opt);
+	ft_print(current->spec, opt);
+	if (current->left != NULL)
+		ft_parkour(curent->left);
+}
