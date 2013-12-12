a0338a214a5f76615a767d7388179c66635e7a67
Nathanael PINEAU <nofigh@hotmail.fr>
2013-12-12 15:02:30 +0100

    correction mineure sur ft_parcour and it's reversed version.

--- sources/tree/ft_parkour.c
+++ sources/tree/ft_parkour.c
@@ -6,7 +6,7 @@
 /*   By: npineau <npineau@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2013/12/12 13:22:35 by npineau           #+#    #+#             */
-/*   Updated: 2013/12/12 13:22:41 by npineau          ###   ########.fr       */
+/*   Updated: 2013/12/12 15:00:57 by npineau          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,8 +15,8 @@
 void	ft_parkour(t_btree *current, t_opt *opt)
 {
 	if (current->left != NULL)
-		ft_parkour(curent->left, opt);
+		ft_parkour(current->left, opt);
 	ft_print(current->spec, opt);
 	if (current->right != NULL)
-		ft_parkour(curent->right);
+		ft_parkour(current->right);
 }
--- sources/tree/ft_rev_parkour.c
+++ sources/tree/ft_rev_parkour.c
@@ -6,7 +6,7 @@
 /*   By: npineau <npineau@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2013/12/12 13:24:55 by npineau           #+#    #+#             */
-/*   Updated: 2013/12/12 13:26:05 by npineau          ###   ########.fr       */
+/*   Updated: 2013/12/12 15:01:21 by npineau          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,8 +15,8 @@
 void	ft_rev_parkour(t_btree *current, t_opt *opt)
 {
 	if (current->right != NULL)
-		ft_parkour(curent->right, opt);
+		ft_parkour(current->right, opt);
 	ft_print(current->spec, opt);
 	if (current->left != NULL)
-		ft_parkour(curent->left);
+		ft_parkour(current->left);
 }
